3663662180e039d7ef86f4d99728ebc0e55c1c0b
Graham Darcey <graham@hackclub.com>
2024-12-09 13:50:17 -0500

    disallow directory listing and default to index.html via witchery

--- serve.py
+++ serve.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
-from http import server # Python 3
+from http import server  # Python 3
 import os
 
-class MyHTTPRequestHandler(server.SimpleHTTPRequestHandler):
-        def end_headers(self):
-                self.send_my_headers()
-                server.SimpleHTTPRequestHandler.end_headers(self)
 
-        def send_my_headers(self):
-                self.send_header("Access-Control-Allow-Origin", "*")
-                self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
-                self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+class MyHTTPRequestHandler(server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = "index.html"
+        return server.SimpleHTTPRequestHandler.do_GET(self)
+
+    def end_headers(self):
+        self.send_my_headers()
+        server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
+        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+
 
 if __name__ == '__main__':
-        server.test(HandlerClass=MyHTTPRequestHandler, port=os.environ.get('PORT', 8080))
+    server.test(HandlerClass=MyHTTPRequestHandler, port=os.environ.get('PORT', 8080))
